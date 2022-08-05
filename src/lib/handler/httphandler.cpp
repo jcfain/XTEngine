f07127a2218d66d3f78ed269d189a4e1c3678c54
Fain <25233821+jcfain@users.noreply.github.com>
2022-08-05 18:55:26 -0400

    Fix for empty VR library path

--- src/lib/handler/httphandler.cpp
+++ src/lib/handler/httphandler.cpp
@@ -538,7 +538,8 @@ HttpPromise HttpHandler::handleVideoStream(HttpDataPtr data)
                     }
                     LogHandler::Debug("Looking for media in library: " + mediaName);
                     QString filename = SettingsHandler::getSelectedLibrary() + "/" + mediaName;
-                    if(mediaName.startsWith(SettingsHandler::getVRLibrary())) {
+                    QString vrLibrary = SettingsHandler::getVRLibrary();
+                    if(!vrLibrary.isEmpty() && QFile::exists(vrLibrary) && mediaName.startsWith(vrLibrary)) {
                         filename = mediaName;
                     }
                     //filename = _videoHandler->transcode(filename);
