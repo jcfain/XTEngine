6bbdf28136646920eed698ff31efa82635804025
Fain <25233821+jcfain@users.noreply.github.com>
2022-08-12 23:39:56 -0400

    Change version

--- src/lib/handler/settingshandler.cpp
+++ src/lib/handler/settingshandler.cpp
@@ -5,8 +5,8 @@ const QMap<TCodeVersion, QString> SettingsHandler::SupportedTCodeVersions = {
     {TCodeVersion::v3, "TCode v0.3"}
 };
 
-const QString SettingsHandler::XTEVersion = QString("0.333a_%1T%2").arg(__DATE__).arg(__TIME__);
-const float SettingsHandler::XTEVersionNum = 0.333f;
+const QString SettingsHandler::XTEVersion = QString("0.334a_%1T%2").arg(__DATE__).arg(__TIME__);
+const float SettingsHandler::XTEVersionNum = 0.334f;
 
 SettingsHandler::SettingsHandler(){}
 SettingsHandler::~SettingsHandler()
