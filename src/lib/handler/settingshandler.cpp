ffc52461aee2c14be55e40b029f521edc6ebe426
Fain <25233821+jcfain@users.noreply.github.com>
2024-05-27 11:19:27 -0400

    Up the version

--- src/lib/handler/settingshandler.cpp
+++ src/lib/handler/settingshandler.cpp
@@ -1,7 +1,7 @@
 #include "settingshandler.h"
 
-const QString SettingsHandler::XTEVersion = "0.452b";
-const float SettingsHandler::XTEVersionNum = 0.451f;
+const QString SettingsHandler::XTEVersion = "0.453b";
+const float SettingsHandler::XTEVersionNum = 0.453f;
 const QString SettingsHandler::XTEVersionTimeStamp = QString(XTEVersion +" %1T%2").arg(__DATE__).arg(__TIME__);
 
 SettingsHandler::SettingsHandler(){}
